cb7a2d0515096786884a98df6d9937697d1d4db1
Naymco <alexsantos2018@outlook.es>
2019-08-10 15:45:24 +0200

    src/api/git.d.ts

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function cambiarClase(){
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+    let menuOpen = document.getElementById('menu-toggle');
+        menuOpen.classList.toggle('menu-open');
+}
